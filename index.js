331d7a48c3ed7fc8b0641c6b93d31177f3c10120
Antonino Manuele Gargiulo <ant.gar98@gmail.com>
2023-01-08 13:15:29 +0100

    heroku config

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const querystring = require('querystring')
 const axios = require('axios')
+const path = require('path');
 
 const app = express();
-const port = 8888;
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
-const FRONTEND_URI = process.env.FRONTEND_URI
+const FRONTEND_URI = process.env.FRONTEND_URI;
+const PORT = process.env.PORT || 8888;
+
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, './client/build')));
 
 /**
  * Generates a random string containing numbers and letters
@@ -77,7 +81,7 @@ app.get('/callback', (req, res) => {
             expires_in
           });
 
-          res.redirect(`http://localhost:3000/?${queryParams}`); // react
+          res.redirect(`${FRONTEND_URI}/?${queryParams}`); // react
         } else {
           res.redirect(`/?${querystring.stringify({ error: 'invalid_token' })}`);
         }
@@ -112,7 +116,12 @@ app.get('/refresh_token', (req, res) => {
       });
 });
 
-app.listen(port, () => {
-  console.log("express app liseting!");
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`express app liseting at port ${PORT} !`);
 });
 
